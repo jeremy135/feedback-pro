1d03546f2e418ee1dd5549e934ad554cfe3c6bcf
root <root@royaldt.com>
2012-05-19 22:41:24 +0900

    fixed feedback_modal js template

--- js/feedback_modal.js
+++ js/feedback_modal.js
@@ -1,23 +1,27 @@
 /**
-* Provide the HTML to create the modal dialog.
-*/
+ * Provide the HTML to create the modal dialog.
+ */
 Drupal.theme.prototype.CToolsFeedbackModal = function () {
-  var html = ''
+    var html = '';
 
-  html += '<div id="ctools-modal">';
- 
-  html += '              <span id="modal-title" class="modal-title"></span>';
-  html += '              <span class="popups-close"><a class="close" href="#"></a></span>';
-  html += '              <div class="clear-block"></div>';
-  html += '            </div>';
-  html += '          <div class="popups-container">';
-  html += '            <div class="modal-scroll"><div id="modal-content" class="modal-content ></div></div>';
-  html += '            </div>';
-  //html += '            <div class="popups-buttons"></div>'; //Maybe someday add the option for some specific buttons.
-  //  html += '            <div class="popups-footer"></div>'; //Maybe someday add some footer.
+    html += '<div id="ctools-modal">';
+    html += '    <div id="feedback-block" class="feedback-block ">';
+    html += '              <span id="modal-title" class="modal-title"></span>';
+    html += '              <span class="popups-close"><a class="close" href="#"></a></span>';
+    html += '              <div class="clear-block"></div>';
+    //html += '            </div>';
+    html += '           <div class="popups-container">';
+    html += '             <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
+    html += '          </div>';
+    html += '         <div class="clearboth"></div>';
+    //html += '          <div class="popups-container">';
+    //html += '            <div class="modal-scroll"><div id="modal-content" class="modal-content ></div></div>';
+    //html += '            </div>';
+    //html += '            <div class="popups-buttons"></div>'; //Maybe someday add the option for some specific buttons.
+    //  html += '            <div class="popups-footer"></div>'; //Maybe someday add some footer.
+    html += ' </div>';
+    html += '</div>';
 
-  html += '</div>';
-
-  return html;
+    return html;
 
 }
